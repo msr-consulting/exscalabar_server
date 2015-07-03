528f31b8afdd3999847bb7e83ba6c60288fa5d39
msrconsultingllc <matt.richardson@msrconsults.com>
2015-07-03 11:01:24 -0600

    Added more developed example for processing data using python.

--- Python/example.py
+++ Python/example.py
@@ -1,9 +1,18 @@
 from tkinter import *
-from tkinter.filedialog import askopenfilenames
+from tkinter.filedialog import askopenfilename
 import pandas as pd
+import matplotlib.pyplot as plt
 
-fname = askopenfilenames()
 
-# df = pd.read_csv(fname, sep='\t')
+fname = askopenfilename()
 
-print(fname)
+df = pd.read_csv(fname, sep='\t')
+df.set_index('Time')
+
+f,ax=plt.subplots()
+
+df['pas.cell_4.IA'].plot()
+ax.set_title('Example of using Python for EXSCALABAR')
+ax.set_xlabel('Time (s)')
+ax.set_ylabel('Cell 4 IA')
+plt.show()
